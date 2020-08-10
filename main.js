e78df7e847fc86ff1359607c99ed0e7f70be81a7
Constantus <38108928+Constantus@users.noreply.github.com>
2020-08-10 12:47:30 -0400

    Added divs for each slide

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+window.onresize = () => { location.reload();} ;
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
 
